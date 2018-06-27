74c8546da859e2b5ad5b35799452ce19cbba302c
ngokhoaanh <ngokhoaanh2509@gmail.com>
2018-06-27 17:29:20 +0700

    intial commit

---
+++ Session01/hello-world.py
@@ -0,0 +1,4 @@
+print("Hello C4T4")
+
+# name = input ( "what's your name?")
+# print ("Hi",name)
---
+++ Session01/yob.py
@@ -0,0 +1,3 @@
+yob = int (input("ban sinh nam bao nhieu"))
+age = 2018 - (yob)
+print("ban",age,"tuoi")
